600fd500cdca838c545f1bff786db6a1cba2b422
taskinoz <tristan@taskinoz.com>
2019-05-13 23:54:03 +1000

    - Added write to CSS file - API settings - Generate CSS

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const fs = require('fs');
 const file = fs.readFileSync('./test-site/index.html','utf8');
 const $ = cheerio.load(file);
 
+const api = {
+  url:"./anylitics.php",
+  active:"a",
+  hover:"h"
+};
+
 var sites = [];
+
+var CSS=`/* CSS Link Tracking */\na[href]:active::after{width:0;height:0}`;
 
 $('a').each(function() {
   sites.push($(this).attr('href'))
 })
 
+for (var i = 0; i < sites.length; i++) {
+  CSS+=`a[href="${sites[i]}"]:active::after{content:url(${api.url}?${api.active}=${sites[i]})}`;
+}
+
 console.log(sites);
+fs.writeFileSync("test.css",CSS);
